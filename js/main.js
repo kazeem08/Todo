ee8d0c93655ba96186871ffe2386d63c52f201bc
Jonathan Adebola <conektbishop@gmail.com>
2018-12-12 11:55:01 +0100

    Functionality of search by text

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+    $('#submit').click(function(){
+        var entry = $('#input').val();
+        
+        $.ajax({
+            type: 'GET',
+            url:'https://jsonplaceholder.typicode.com/todos',
+            
+            success: function(data){
+                let title = this.title
+                //let character = title.includes(entry);
+                for(let i of data){
+                    let character = i.title.includes(entry);
+                    if(character){
+                         console.table(i);
+                        //$('#output').append(i);
+                    }
+
+                }
+                
+            }
+
+        })
+        
+    });
+
+    $('#submitForId').click(function(){
+
+         entryId = $('#inputNumber').val();
+
+        $.ajax({
+            type: 'GET',
+            url:`https://jsonplaceholder.typicode.com/todos/${entryId}`,
+
+            success: function(data){
+                console.table(data)
+            }
+
+        })
+
+    })
+
+});
+
